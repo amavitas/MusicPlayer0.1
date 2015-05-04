f35b25aab64874807b9cf54bf0869e9ea8380fd3
Attila Mavitas <attila.mavitas@gmail.com>
2015-05-05 00:48:00 +0200

    v0.1

--- app/src/main/java/com/musicplayer/peacocksmith/musicplayer01/MusicPlayer.java
+++ app/src/main/java/com/musicplayer/peacocksmith/musicplayer01/MusicPlayer.java
@@ -1,38 +1,91 @@
 package com.musicplayer.peacocksmith.musicplayer01;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+package com.example.musicplayer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.view.Menu;
+import android.widget.ListView;
 
 
-public class MusicPlayer extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    //song list variables
+    private ArrayList<Song> songList;
+    private ListView songView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_music_player);
+        setContentView(R.layout.activity_main);
+
+        //retrieve list view
+        songView = (ListView)findViewById(R.id.song_list);
+        //instantiate list
+        songList = new ArrayList<Song>();
+        //get songs from device
+        getSongList();
+        //sort alphabetically by title
+        Collections.sort(songList, new Comparator<Song>() {
+            public int compare(Song a, Song b) {
+                return a.getTitle().compareTo(b.getTitle());
+            }
+        });
+        //create and set adapter
+        SongAdapter songAdt = new SongAdapter(this, songList);
+        songView.setAdapter(songAdt);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_music_player, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    //method to retrieve song info from device
+    public void getSongList(){
+        //query external audio
+        ContentResolver musicResolver = getContentResolver();
+        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+        //iterate over results if valid
+        if(musicCursor!=null && musicCursor.moveToFirst()){
+            //get columns
+            int titleColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.ARTIST);
+            //add songs to list
+            do {
+                long thisId = musicCursor.getLong(idColumn);
+                String thisTitle = musicCursor.getString(titleColumn);
+                String thisArtist = musicCursor.getString(artistColumn);
+                songList.add(new Song(thisId, thisTitle, thisArtist));
+            }
+            while (musicCursor.moveToNext());
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
---
+++ app/src/main/java/com/musicplayer/peacocksmith/musicplayer01/SongAdapter.java
@@ -0,0 +1,7 @@
+package com.musicplayer.peacocksmith.musicplayer01;
+
+/**
+ * Created by Peacock Smith on 5/5/2015.
+ */
+public class SongAdapter {
+}
